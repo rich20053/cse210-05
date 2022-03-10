851fb365c8acd71c8ffb35006ebca3b34c999949
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-03-09 20:15:00 -0700

    Mark keyboard_service update

--- game/services/keyboard_service.py
+++ game/services/keyboard_service.py
@@ -1,4 +1,5 @@
 import pyray
+from raylib import KEY_FIVE, KEY_FOUR, KEY_ONE, KEY_THREE, KEY_TWO
 from game.shared.point import Point
 
 
@@ -25,20 +26,35 @@ class KeyboardService:
         self._keys['k'] = pyray.KEY_K
         self._keys['l'] = pyray.KEY_L
 
+        self._keys['n'] = pyray.KEY_N
+
+        self._keys['1'] = KEY_ONE
+        self._keys['2'] = KEY_TWO
+        self._keys['3'] = KEY_THREE
+        self._keys['4'] = KEY_FOUR
+        self._keys['5'] = KEY_FIVE
+
+
+
     def is_key_up(self, key):
         """Checks if the given key is currently up.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d or i, j, k, l or n)
         """
-        pyray_key = self._keys[key.lower()]
+        if (key.isdigit()):
+            return pyray.is_key_up(self._keys[key])
+        pyray_key = self._keys[key]
         return pyray.is_key_up(pyray_key)
 
     def is_key_down(self, key):
         """Checks if the given key is currently down.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d or i, j, k, l or n)
         """
-        pyray_key = self._keys[key.lower()]
+        if (key.isdigit()):
+            return pyray.is_key_down(self._keys[key])
+        pyray_key = self._keys[key]
         return pyray.is_key_down(pyray_key)
+            
